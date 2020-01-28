a77eebe6c93232789e9702a028c03b46082973b4
Emad Iddin Ali <emal02002@utb.vaxjo.se>
2020-01-28 17:52:41 +0100

    lite justerimgar

--- BubbleSort/BubbleSort/Program.cs
+++ BubbleSort/BubbleSort/Program.cs
@@ -10,14 +10,14 @@ namespace BubbleSort
 
         static void Main(string[] args)
         {
-            int[] sizes = new int[] { 10, 1000, 100000 }; // Det är en array med olika storlekar
-            for (int i = 0; i < sizes.Length; i++)
+            int[] strolekar = new int[] { 10, 1000, 100000 }; // Det är en array med olika storlekar
+            for (int i = 0; i < strolekar.Length; i++) 
             {
-                Console.WriteLine("Skapar slumpad data av längd " + sizes[i]); //Skirver hur många tal som skall sorteras
-                int[] data = GenerateData(sizes[i]); // skapar en array som generarar element och skpar en metod
+                Console.WriteLine("Skapar slumpad data av längd " + strolekar[i]); //Skirver hur många tal som skall sorteras
+                int[] lista = GenerateData(strolekar[i]); // skapar en array som generarar element och skpar en metod
                 Console.WriteLine("Sorterar slumpad data");
                 DateTime startTid = DateTime.Now; // Tiden för när sorteringen börjar
-                BubbleSort(data);
+                BubbleSort(lista); //Skapar en metod för bubble sort for att sortera listan 
                 TimeSpan deltaTid = DateTime.Now - startTid;
                 Console.WriteLine("Det tog {0:0.00} ms att sortera.\n", deltaTid.TotalMilliseconds); // \n Tar en ny tom rad
                 
